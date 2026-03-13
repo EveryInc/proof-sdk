28d6db32fab2acd0549d95071516d2b1ed9149bb
Trevin Chow <trevin@trevinchow.com>
2026-03-12 20:51:28 -0700

    refactor(server): move share web routes into package

--- packages/doc-server/src/share.ts
+++ packages/doc-server/src/share.ts
@@ -1,5 +1,5 @@
 import type { Router } from 'express';
-import { shareWebRoutes } from '../../../server/share-web-routes.js';
+import { shareWebRoutes } from './share-web-routes.js';
 
 export function createShareRouter(): Router {
   return shareWebRoutes;
